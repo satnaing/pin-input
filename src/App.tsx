6aff332732bc212a95381b23db7452f9b48ddb39
satnaing <satnaingdev@gmail.com>
2024-09-01 15:27:33 +0700

    feat: add pin-input form component

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import {
 import { pinInputCodes } from "./assets/pin-input-codes";
 import { cn } from "./lib/utils";
 import useCopy from "./hooks/use-copy";
+import { PinInputForm } from "./components/pin-input-form";
 
 export default function App() {
   const [copied, setCopied] = useCopy();
@@ -23,8 +24,8 @@ export default function App() {
       <div className="space-y-1">
         <h1 className="font-bold text-2xl text-center">Pin Input</h1>
         <p className="text-muted-foreground text-center text-lg leading-tight">
-          A modular, style-agnostic PinInput component, ready to use <br /> with
-          just a copy-paste—no extra libraries needed.
+          A modular, style-agnostic PinInput component for React, <br />
+          ready to use with just a copy-paste—no extra libraries needed.
         </p>
       </div>
       <div className="space-x-4 mt-4 text-center">
@@ -35,14 +36,21 @@ export default function App() {
         <ThemeSwitch />
       </div>
 
-      <section className="grid grid-cols-12 grid-rows-4 gap-4 mt-8">
+      <section className="grid grid-cols-12 grid-rows-6 gap-4 mt-8">
         {/* Pin Input */}
-        <div className="bg-card p-4 rounded-md shadow-sm col-span-7 row-span-4">
-          <h2 className="font-medium text-lg mb-1">Pin Input</h2>
+        <div className="bg-card p-4 rounded-md shadow-sm col-span-7 row-span-4 space-y-2">
+          <h2 className="font-medium text-lg mb-1">
+            Two-factor Authentication
+          </h2>
+          <p className="text-muted-foreground">
+            Please enter the authentication code. <br />
+            We have sent the authentication code to your email.
+          </p>
+          <PinInputForm />
         </div>
 
         {/* Get Started */}
-        <div className="bg-card p-4 rounded-md shadow-sm col-span-5 row-span-2">
+        <div className="bg-card p-4 rounded-md shadow-sm col-span-5 row-span-3">
           <h2 className="font-medium text-lg mb-1">Get Started</h2>
           <ol className="list-decimal pl-4 text-muted-foreground">
             <li>Install shadcn/ui incl. the Input component.</li>
@@ -52,7 +60,7 @@ export default function App() {
         </div>
 
         {/* Features */}
-        <div className="bg-card p-4 rounded-md shadow-sm col-span-5 row-span-2">
+        <div className="bg-card p-4 rounded-md shadow-sm col-span-5 row-span-3">
           <h2 className="font-medium text-lg mb-1">Features</h2>
           <ul className="list-disc pl-4 text-muted-foreground">
             <li>No additional library</li>
@@ -63,9 +71,18 @@ export default function App() {
           </ul>
         </div>
 
+        {/* Note */}
+        <div className="bg-card p-4 rounded-md shadow-sm col-span-7 row-span-2">
+          <h2 className="font-medium text-lg mb-1">Note</h2>
+          <p className="text-sm text-muted-foreground">
+            You don't need to install component library or any CSS library.
+            However, you can make use of it for the best version available.
+          </p>
+        </div>
+
         {/* Snippets */}
         <div className="bg-card p-4 rounded-md shadow-sm col-span-12 row-span-2">
-          <h2 className="font-medium text-lg mb-1">Snippets</h2>
+          <h2 className="font-medium text-lg mb-2">Snippets</h2>
           <Collapsible defaultOpen>
             <CollapsibleTrigger asChild>
               <Button
